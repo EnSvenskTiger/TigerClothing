192da3c275cf4abefa8b976ae72875d2c568cb73
Malin Lindblom <Malin.Lindblom@LAPTOP-9U8J83ML>
2023-05-15 09:33:31 +0200

    Add project files.

---
+++ TigerClothing/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TigerClothing.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
